0cd52e4a5d2c55527e2ce9239f26074e56a7db54
cassiboy1203 <casvstb@gmail.com>
2022-09-16 11:11:11 +0200

    chore: setup spring boot project

---
+++ src/main/java/com/cassiboy/dont_play_with_bot/DontPlayWithBotApplication.java
@@ -0,0 +1,13 @@
+package com.cassiboy.dont_play_with_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontPlayWithBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontPlayWithBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cassiboy/dont_play_with_bot/DontPlayWithBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassiboy.dont_play_with_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontPlayWithBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
