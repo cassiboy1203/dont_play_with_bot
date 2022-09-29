6eabd95a1c60118f5b6511f311811edbae2ec110
cassiboy1203 <casvstb@gmail.com>
2022-09-29 14:39:39 +0200

    fix: get characters now works when more then 10 characters are registered (part1)

--- src/main/java/com/cassiboy/dont_play_with_bot/commands/GetCharactersCommand.java
+++ src/main/java/com/cassiboy/dont_play_with_bot/commands/GetCharactersCommand.java
@@ -1,13 +1,16 @@
 package com.cassiboy.dont_play_with_bot.commands;
 
 import com.cassiboy.dont_play_with_bot.dao.ICharacterDAO;
+import com.cassiboy.dont_play_with_bot.dto.PlayerCharacter;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.entity.Message;
 import discord4j.core.spec.EmbedCreateSpec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class GetCharactersCommand implements ISlashCommand {
@@ -26,6 +29,13 @@ public class GetCharactersCommand implements ISlashCommand {
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
+        return event.deferReply()
+                .withEphemeral(true)
+                .then(sendMessage(event))
+                .then();
+    }
+
+    public Mono<Message> sendMessage(ChatInputInteractionEvent event){
         var charClass = getStringValue(event, "class", null);
         var server = getStringValue(event, "server", null);
         var stronghold = getStringValue(event, "stronghold", null);
@@ -33,9 +43,8 @@ public class GetCharactersCommand implements ISlashCommand {
         var characters = characterDAO.getCharactersInList(charClass, server, stronghold);
 
         var embeds = new ArrayList<EmbedCreateSpec>();
+
         for (var character: characters){
-
-
             var reasons = characterDAO.getReasons(character.getName());
             var sb = new StringBuilder();
             sb.append("**Reasons** \n");
@@ -54,15 +63,30 @@ public class GetCharactersCommand implements ISlashCommand {
                     .addField("stronghold", character.getStronghold(), true);
 
             embeds.add(embedBuilder.build());
+
+            if (embeds.size() == 10){
+                event.createFollowup()
+                        .withEmbeds(embeds)
+                        .withEphemeral(true)
+                        .subscribe();
+
+                embeds = new ArrayList<>();
+            }
         }
 
-        if (embeds.isEmpty()){
-            return event.reply("There are no characters with the selected filters registered.")
+        if (!embeds.isEmpty()){
+            event.createFollowup()
+                    .withEmbeds(embeds)
+                    .withEphemeral(true)
+                    .subscribe();
+        }
+
+        if (characters.isEmpty()){
+            return event.createFollowup("There are no characters with the selected filters registered.")
                     .withEphemeral(true);
         }
 
-        return event.reply()
-                .withEmbeds(embeds)
+        return event.createFollowup("All characters returned.")
                 .withEphemeral(true);
     }
 }
